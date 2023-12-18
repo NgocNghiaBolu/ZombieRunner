1bb52c30b0d6243a85e459b3e860f1d8292ed982
NgocNghiaBolu <nghia048@gmail.com>
2023-12-18 15:01:48 +0700

    init commit

---
+++ Assets/Script/Enemy/EnemyHeath.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyHeath : MonoBehaviour
+{
+    [SerializeField] float HitPoint = 200f;
+    public float ShootTime = 0.2f;
+    public AudioClip AuZombDie;
+
+    //private bool isDead = false;
+    public static AudioSource AuzombRun;//static de tham chieu qua PlayerHealth tat sound
+    private Animator animGetHit;
+    private bool isShooten;
+
+    public bool IsShooten //Dong goi
+    {
+        get { return isShooten; }
+        set
+        {
+            isShooten = value;
+            SetGetHit(isShooten);
+            UpdateShootenTime();
+        }
+    }
+
+    private float ShootenLastTime = 0f;
+    void Start()
+    {
+        AuzombRun = GetComponent<AudioSource>();
+        animGetHit = GetComponent<Animator>();
+        IsShooten = false;
+    }
+
+    void UpdateShootenTime()
+    {
+        ShootenLastTime = Time.time;
+    }
+
+    void SetGetHit(bool isShooten)
+    {
+        animGetHit.SetBool("getHit", isShooten);
+    }
+
+    bool isDie = false;
+    public bool IsDied()
+    {
+        return isDie;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        BroadcastMessage("OnDamageEnemy");
+        AuzombRun.Play();
+        IsShooten = true;
+        HitPoint -= damage;//ban trung tru di so diem quy dinh
+        if (HitPoint <= 0)
+        {
+            if (EnemyHeath.AuzombRun != null)
+            {
+                EnemyHeath.AuzombRun.Stop();
+                EnemyHeath.AuzombRun.PlayOneShot(AuZombDie);
+            }
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDie) return;
+        isDie = true;
+        GetComponent<Animator>().SetTrigger("Die");
+
+        Invoke(nameof(LoadNewScene), 5f);
+    }
+
+    void LoadNewScene()
+    {
+        // Load your desired scene by name
+        SceneManager.LoadScene("Scene2");
+    }
+
+    private void Update()
+    {
+        if (IsShooten && Time.time >= ShootenLastTime + ShootTime)
+        {
+            IsShooten = false;
+        }
+    }
+}
